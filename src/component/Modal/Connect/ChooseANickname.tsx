4436632cf6e0fe850835ea136864e367b6af3611
ayoeze191 <ayoeze191@gmail.com>
2023-07-04 19:01:48 +0100

    adviced

--- src/component/Modal/Connect/ChooseANickname.tsx
+++ src/component/Modal/Connect/ChooseANickname.tsx
@@ -6,7 +6,7 @@ const ChooseANickname = () => {
           <input type="text" placeholder='ENTER YOUR NICKNAME' className='font-[400] text-[20px] font-droid leading-[23.61px] py-[24px] px-[40px] text-[#8C8C8C] rounded-t-[16px] bg-[#032449] md:w-[416px] mx-auto text-center' />  
     <div className='flex flex-col gap-1 font-Archivo_Regular font-normal'>
         <p className='leading-[21.76px]  text-[20px] text-center'>
-        You are advice to use the same discord
+        You are adviced to use the same discord
         </p>
         <p className='flex justify-center leading-[21.76px] gap-2 text-[20px] text-center'>
         username for easy stat tracking.
