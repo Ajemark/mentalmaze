5e2a9636a820f7c52ae6e31a20a9edde8550ef52
ayoeze191 <ayoeze191@gmail.com>
2023-07-07 18:25:41 +0100

    asas

--- src/component/ui/Sidebar.tsx
+++ src/component/ui/Sidebar.tsx
@@ -5,16 +5,20 @@ import nav3 from "./../../assets/sidebar/nav3.png"
 import discord from "./../../assets/sidebar/discord.svg"
 import telegram from "./../../assets/sidebar/telegram.svg"
 import twitter from "./../../assets/sidebar/twitter.svg"
+import { useNavigate } from "react-router-dom"
+
 
 
 
 const Sidebar = () => {
+  const navigate = useNavigate()
+
   return (
     <div className='w-[104px] flex flex-col justify-start gap-56 py-[46px] relative bg-[#010C18] z-20  opacity-[0.4000000059604645] items-center  mt-[104px]'>
         <div className='flex flex-col gap-8'>
-          {[nav1, nav2, nav3].map((src) => {
-            return <div className='w-fit'>
-            <img src={src} /> 
+          {[{image: nav1, title: "/"}, {image: nav2, title: '/leadership'},{image: nav3, title: "/"}].map((src) => {
+            return <div className='w-fit' onClick={() => navigate(src.title)}>
+            <img src={src.image} /> 
           </div>
           })}
         </div>
