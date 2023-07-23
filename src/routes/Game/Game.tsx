4713f4ce62f9b70518ddee4e5d35c4d9c45059a9
ayoeze191 <ayoeze191@gmail.com>
2023-07-23 15:22:56 +0100

    asas

--- src/routes/Game/Game.tsx
+++ src/routes/Game/Game.tsx
@@ -4,7 +4,7 @@ const Game = () => {
   return (
     <div className='relative px-[52px]'>
         <div></div>
-        <div className='flex gap-[38px]'>
+        <div className='flex gap-[38px] flex-col md:flex-row'>
           <Rating />
           <About />
         </div>
@@ -16,7 +16,7 @@ const Game = () => {
 
 const Rating = () => {
   return (
-    <div className='py-[24px] w-[266px] border-blue-80 border-solid border-[4px] rounded-[24px]'>
+    <div className='py-[24px] w-full md:w-[266px] border-blue-80 border-solid border-[4px] rounded-[24px]'>
        <p className='h-[86px] w-full border-b-[4px] border-b-blue-80 mb-[32px]'></p>
       
         <div className=''>
@@ -47,7 +47,7 @@ const Rating = () => {
 
 const About = () => {
   return (
-    <div className='rounded-[24px] border-[4px] border-blue-80 flex-1'>
+    <div className=' rounded-[24px] border-[4px] border-blue-80 w-full md:w-fit md:flex-1'>
       <p className='text-[32px] font-droid text-white text-center pt-[16px] pb-[32px] border-b-[4px] border-b-blue-80'>MATH PUZZLE</p>
       <div className='px-[40px] pt-[32px]'>
       <p className='text-white  text-[20px] leading-[31.76px] font-Archivo_Regular '>
