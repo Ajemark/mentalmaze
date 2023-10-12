e659dba941433bd570099ad2bf6e90cd675ccb83
Ajemark <ajemark0110@gmail.com>
2023-10-12 23:14:51 +0100

    Game flow working 07

--- src/routes/Creator/settings/steps/Submit.tsx
+++ src/routes/Creator/settings/steps/Submit.tsx
@@ -1,12 +1,15 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../../../../context/UserContext";
 import { useNavigate } from "react-router-dom";
+import ReactLoading from 'react-loading';
+
 
 const Submit = () => {
 
   const navigate = useNavigate()
-  const { questionObj, userDetails, setLoading }: any = useContext(UserContext)
+  const { questionObj, userDetails }: any = useContext(UserContext)
   const [errorMessage, setErrorMessage] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const submitGameApplication = async () => {
     setLoading(true)
@@ -51,26 +54,36 @@ const Submit = () => {
 
   return (
     <div className='px-[16px] md:px-[48px]'>
-      <div className='text-[16px] md:text-[20px] font-Archivo_Regular  text-white  flex flex-col gap-6 mt-[48px] font-[200]'>
-        <p className=' md:leading-[31.76px]'> Hi there!</p>
-        <p className=' md:leading-[31.76px]'> I’m sure you can’t wait to have your game published. So are we, congratulations creator.</p>
+      {
+        loading ? (
+          <div className="flex items-center h-[35vh] justify-center w-full ">
+            <ReactLoading
+              type='spin' color='#0B77F0' height={60} width={37} />
+          </div>) : (
+          <div>
+            <div className='text-[16px] md:text-[20px] font-Archivo_Regular  text-white  flex flex-col gap-6 mt-[48px] font-[200]'>
+              <p className=' md:leading-[31.76px]'> Hi there!</p>
+              <p className=' md:leading-[31.76px]'> I’m sure you can’t wait to have your game published. So are we, congratulations creator.</p>
 
-        <p className=' md:leading-[31.76px]'> To be sure, your game meets all of our requirements, we will have the judges go through the game. Once they have verified the authenticity of your game guess what? It will be live.</p>
+              <p className=' md:leading-[31.76px]'> To be sure, your game meets all of our requirements, we will have the judges go through the game. Once they have verified the authenticity of your game guess what? It will be live.</p>
 
-        <p className=' md:leading-[31.76px]'> And if otherwise, you have nothing to worry about, your deposit of 3 Aurora Token, will be refunded to your wallet.</p>
+              <p className=' md:leading-[31.76px]'> And if otherwise, you have nothing to worry about, your deposit of 3 Aurora Token, will be refunded to your wallet.</p>
 
-        <p className=' md:leading-[31.76px]'> If you need further assistance, do not hesitate to reach us on discord. Our response team, will be happy to answer any question you might have.</p>
+              <p className=' md:leading-[31.76px]'> If you need further assistance, do not hesitate to reach us on discord. Our response team, will be happy to answer any question you might have.</p>
 
-        <p className=' md:leading-[31.76px]'> Thank you, Creator! Goodluck on your application.</p>
-      </div>
-      <div className=" flex flex-col gap-[24px] mt-[48px] w-full ">
-        <button
-          onClick={submitGameApplication}
-          className="w-full bg-blue-50 text-white text-[15px] font-Archivo_Regular rounded-[16px] border-[2px] border-blue-main py-[16px] ">
-          SUBMIT APPLICATION
-        </button>
-      </div>
-      {errorMessage != '' && <p className="text-red-500 text-center">{errorMessage}</p>}
+              <p className=' md:leading-[31.76px]'> Thank you, Creator! Goodluck on your application.</p>
+            </div>
+            <div className=" flex flex-col gap-[24px] mt-[48px] w-full ">
+              <button
+                onClick={submitGameApplication}
+                className="w-full bg-blue-50 text-white text-[15px] font-Archivo_Regular rounded-[16px] border-[2px] border-blue-main py-[16px] ">
+                SUBMIT APPLICATION
+              </button>
+            </div>
+            {errorMessage != '' && <p className="text-red-500 text-center">{errorMessage}</p>}
+          </div>
+        )
+      }
     </div>
   )
 }
